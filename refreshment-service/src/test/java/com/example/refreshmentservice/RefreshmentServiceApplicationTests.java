ad4f3addc09f02b009ae7c4aaa02ebbe2590bd72
sendkumaranil <anil.verma4283@gmail.com>
2020-07-24 18:52:51 +0530

    addedd refreshment service

---
+++ refreshment-service/src/test/java/com/example/refreshmentservice/RefreshmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.refreshmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
